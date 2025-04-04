475db1db3ba6ad8e4c7d3273db94ecdd5e098e4a
Caldipawell <adamsembiring01@gmail.com>
2025-04-04 18:40:54 +0700

    models for each roles

--- main/models.py
+++ main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+domicile_choices = [
+    ("jakut", "Jakarta Utara"),
+    ("jakbar", "Jakarta Barat"),
+    ("jaktim", "Jakarta Timur"),
+    ("jakpus", "Jakarta Pusat"),
+    ("jaksel", "Jakarta Selatan"),
+    ("depok", "Depok"),
+    ("bekasi_kota", "Bekasi (Kota)"),
+    ("bekasi_kab", "Bekasi (Kabupaten)"),
+    ("bogor_kota", "Bogor (Kota)"),
+    ("bogor_kab", "Bogor (Kabupaten)"),
+    ("tangsel", "Tangerang Selatan"),
+    ("tangerang_kota", "Tangerang (Kota)"),
+    ("tangerang_kab", "Tangerang (Kabupaten)"),
+]
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    nomor_hp = models.CharField(max_length=16)
+    email = models.EmailField(max_length=50)
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=50)
+    nomor_hp = models.CharField(max_length=16)
+    wallet = models.OneToOneField(Wallet, on_delete=models.CASCADE)
+    domicile = models.CharField(max_length=100, choices=domicile_choices, default="Domisili kamu dimana", blank=False)
+
+class Worker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    nomor_hp = models.CharField(max_length=16)
+    email = models.EmailField(max_length=50)
+    wallet = models.OneToOneField(Wallet, on_delete=models.CASCADE)
+    domicile = models.CharField(max_length=100, choices=domicile_choices, default="Domisili kamu dimana", blank=False)
+    rating = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(0, 6)],default=0)

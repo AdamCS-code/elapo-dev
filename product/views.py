c21217840318f03904b5578f9abedfa6b9ff9493
Adam <adamsembiring01@gmail.com>
2025-04-05 20:14:30 +0700

    create view for show all product avai

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Product
 # Create your views here.
+def all_product(request):
+    products = Product.objects.all()
+    product_json = [
+        {
+            'id' : product.id,
+            'product_name': product.product_name,
+            'stock': product.stock,
+            'price': product.price,
+            'description': product.description,
+        } for product in products
+    ]
+    return JsonResponse({'products' : product_json})

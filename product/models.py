ebcb5bdb24713f0ddf34b2d6cc16013595346ae6
Adam <adamsembiring01@gmail.com>
2025-04-05 20:07:47 +0700

    create model for product

--- product/models.py
+++ product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True)
+    product_name = models.CharField(max_length=69)
+    price = models.DecimalField()
+    description = models.CharField(max_length=150)
+    stock = models.IntegerField()

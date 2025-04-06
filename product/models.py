267a1670637a59de46b4b8d1a22de46df12dac14
Adam <adamsembiring01@gmail.com>
2025-04-06 07:42:48 +0700

    fixing, migrate, test

--- product/models.py
+++ product/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Product(models.Model):
     id = models.UUIDField(primary_key=True)
     product_name = models.CharField(max_length=69)
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=6, decimal_places=6)
     description = models.CharField(max_length=150)
     stock = models.IntegerField()
--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import all_product
 
 appname = 'product'
 
 urlpatterns = [
-    
+    path('all_product', all_product, name='all_product'),
 ]

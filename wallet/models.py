91b7bd4597697bfaf7ae8d6add195005720d3b17
Adam <adamsembiring01@gmail.com>
2025-04-06 13:11:53 +0700

    create models for wallet

--- wallet/models.py
+++ wallet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Wallet(models.Model):
+    id = models.UUIDField(primary_key=True)
+    user = models.OneToOneField(User)
+    saldo = models.DecimalField(max_digits=12, decimal_places=0, default=0)

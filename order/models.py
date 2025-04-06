9c0e4d3b37cd0f28cf608f0b3feb778166d1ef86
Adam <adamsembiring01@gmail.com>
2025-04-06 13:43:56 +0700

    create models for order

--- order/models.py
+++ order/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cart.models import Cart
 
-# Create your models here.
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, related_name='order_id')
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=12, decimal_places=0, default=0)
+    created_at = models.DateField()
+    status = models.ForeignKey(OrderStatus, on_delete=models.CASCADE)
+
+class OrderStatus(models.Model):
+    id = models.UUIDField(primary_key=True, related_name='order_status_id')
+    status = models.CharField(max_length=30)

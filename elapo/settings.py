ce954807a8fc2647b7fcf99c175a519ffd118888
Adam <adamsembiring01@gmail.com>
2025-04-17 10:32:47 +0700

    create docker script

--- elapo/settings.py
+++ elapo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k1fvhy8q6abe2-fq6p4o3y%7&sq4d0#s)@y^zs-%kxfhzzf(+6'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUT = os.getenv('DEBUG', 'False') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
